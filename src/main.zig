1138e75e17f0facedf64eae9383949392b1dd7da
Gordan Zovko <gordan@zovko.rs>
2025-03-20 00:25:38 +0200

    Basic argument parsing implemented

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,39 @@
 const std = @import("std");
 const root = @import("root.zig");
 
+const InputType = enum { string, file };
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
+
+    const args = try std.process.argsAlloc(alloc);
+    defer std.process.argsFree(alloc, args);
+
+    var input_type: InputType = undefined;
+    if (args.len == 2) {
+        input_type = InputType.string;
+    } else if (args.len == 3) {
+        if (std.mem.eql(u8, args[1], "--string") or std.mem.eql(u8, args[1], "-s")) {
+            input_type = InputType.string;
+        } else if (std.mem.eql(u8, args[1], "--file") or std.mem.eql(u8, args[1], "-f")) {
+            input_type = InputType.file;
+            return error.NotImplemented;
+        } else {
+            std.debug.print("Invalid argument! Expected input type, received {s}\n", .{args[1]});
+            return error.InvalidArgument;
+        }
+    }
+
     const base64 = root.Base64.init();
-    const input_string = "Gami";
-    const result = try base64.encode(alloc, input_string);
-    defer alloc.free(result);
-    std.debug.print("test encode string \"{s}\": {s}\n", .{ input_string, result });
+
+    switch (input_type) {
+        InputType.string => {
+            const result = try base64.encode(alloc, args[1]);
+            defer alloc.free(result);
+            std.debug.print("{s}\n", .{result});
+        },
+        else => {},
+    }
 }
