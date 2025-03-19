f3a82301688210bc0866d52fdf441c978ce5d98c
Gordan Zovko <gordan@zovko.rs>
2025-03-19 23:42:29 +0200

    Properly clean up the pointers

--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,11 @@ const root = @import("root.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
     const alloc = gpa.allocator();
     const base64 = root.Base64.init();
     const input_string = "Gami";
     const result = try base64.encode(alloc, input_string);
+    defer alloc.free(result);
     std.debug.print("test encode string \"{s}\": {s}", .{ input_string, result });
 }
